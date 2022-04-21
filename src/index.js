c1bb26b0655dafcd99c1cd95f27fa04778cbfab0
Marcos <marcosalves@weg.net>
2022-04-21 19:43:09 -0300

    redux persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import store from './store/store';
-import HomeView from './views/Home.view'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import { persistedStore, store } from "./store/store";
+import HomeView from "./views/Home.view";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HomeView />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistedStore}>
+                <HomeView />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { combineReducers, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import CalculatorReducer from "./Calculator/Calculator.reducer";
 import ProductsReducer from "./Products/Products.reducer";
 
@@ -7,6 +10,13 @@ const rootReducer = combineReducers({
     products: ProductsReducer,
 });
 
-const store = createStore(rootReducer);
+const persistedReducer = persistReducer(
+    {
+        key: "root",
+        storage,
+    },
+    rootReducer
+);
 
-export default store;
+export const store = createStore(persistedReducer);
+export const persistedStore = persistStore(store);
